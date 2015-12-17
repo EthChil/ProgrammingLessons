7b9cf06d81d4359f473dcc3d3a8f2b36acbaf3bb
EthChil <ethanchilderhose@gmail.com>
2015-12-16 21:51:43 -0500

    Removal unused imports

--- Lessons/src/org/usfirst/frc/team1360/robot/OI.java
+++ Lessons/src/org/usfirst/frc/team1360/robot/OI.java
@@ -1,8 +1,7 @@
 package org.usfirst.frc.team1360.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-import org.usfirst.frc.team1360.robot.commands.ExampleCommand;
+
 
 /**
  * This class is the glue that binds the controls on the physical operator
